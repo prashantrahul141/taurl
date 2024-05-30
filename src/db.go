222ab3f464cabb4f8f0977b150d9aa426bddf05e
Prashant <prashantrahul141@protonmail.com>
2024-05-30 17:23:42 +0530

    add caching to db

--- src/db.go
+++ src/db.go
@@ -11,7 +11,7 @@ type Urls struct {
 	ID          uint
 	ShortendUrl string
 	OriginalUrl string
-	UniqueId    string
+	UniqueId    string `gorm:"unique"`
 	CreatedAt   time.Time
 }
 
@@ -29,41 +29,65 @@ func setupDbInstance() *gorm.DB {
 
 type DbManager struct {
 	Db *gorm.DB
+	// for in memory caching
+	Cache map[string]Urls // UniqueId: UrlsObject
 }
 
 func SetupDb() DbManager {
-	return DbManager{Db: setupDbInstance()}
+	return DbManager{Db: setupDbInstance(), Cache: make(map[string]Urls)}
+}
 
+func (manager *DbManager) get_from_cache(key string) Urls {
+	return manager.Cache[key]
 }
 
 // Gets url from the db using shortend url.
 func (manager *DbManager) get_url(shortend_url string) (*Urls, error) {
-	var url Urls
+	var url Urls = manager.get_from_cache(shortend_url)
+
+	// we found from cache
+	if url.ID != 0 {
+		return &url, nil
+	}
+
 	result := manager.Db.Where("shortend_url = ?", shortend_url).First(&url)
 	if result.Error != nil {
 		return nil, result.Error
 	}
+
+	manager.Cache[url.UniqueId] = url
 	return &url, nil
 }
 
 // Gets url from the db using unique id.
 func (manager *DbManager) get_url_from_id(uniqueId string) (*Urls, error) {
-	var url Urls
+	var url Urls = manager.get_from_cache(uniqueId)
+
+	// we found from cache
+	if url.ID != 0 {
+		return &url, nil
+	}
+
 	result := manager.Db.Where("unique_id = ?", uniqueId).First(&url)
 	if result.Error != nil {
 		return nil, result.Error
 	}
+
+	// cache before returning
+	manager.Cache[url.UniqueId] = url
 	return &url, nil
 }
 
 // sets url in the db.
 func (manager *DbManager) set_url(original_url string) (*Urls, error) {
-	// create new url.
 	uniqueId := shorten_url(original_url)
+
 	new_url := Urls{ShortendUrl: "http://localhost:3000/" + uniqueId, OriginalUrl: original_url, UniqueId: uniqueId}
 	result := manager.Db.Create(&new_url)
 	if result.Error != nil {
 		return nil, result.Error
 	}
+
+	manager.Cache[uniqueId] = new_url
 	return &new_url, nil
 }
