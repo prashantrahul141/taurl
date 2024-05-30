b687d0ee4b24cf085286db22b8a18b296d922aa6
Prashant <prashantrahul141@protonmail.com>
2024-05-30 16:34:29 +0530

    add direct endpoint

--- src/pages.go
+++ src/pages.go
@@ -1,15 +1,33 @@
 package src
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-func Index(c *gin.Context) {
+func (app *App) Index(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{"title": "Taurl"})
 }
 
-func Redirect(c *gin.Context) {
+type RedirectReq struct {
+	Id string `uri:"id" binding:"required"`
+}
 
+func (app *App) Redirect(c *gin.Context) {
+	var req RedirectReq
+	if err := c.ShouldBindUri(&req); err != nil {
+		c.JSON(400, gin.H{"message": err.Error()})
+		return
+	}
+
+	fmt.Println("req:", req.Id)
+	url, err := app.Db.get_url_from_id(req.Id)
+	if err != nil {
+		c.HTML(http.StatusNotFound, "404.tmpl", gin.H{})
+		return
+	}
+
+	c.Redirect(http.StatusTemporaryRedirect, url.OriginalUrl)
 }
