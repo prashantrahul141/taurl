16e3fcd7ea273b4a3636252eadabaacd33a3db96
Prashant <prashantrahul141@protonmail.com>
2024-05-30 19:09:21 +0530

    add index tmpl

--- assets/static/index.js
+++ assets/static/index.js
@@ -1,11 +1,25 @@
 const formElement = document.getElementById("FORM");
+const inputElement = document.getElementById("INPUT");
+const shortendElement = document.getElementById("SHORTEND");
+const shortendTextElement = document.getElementById("SHORTEND_TEXT");
+const buttonElement = document.getElementById("SUBMIT_BUTTON");
+
+shortendElement.style.opacity = 0;
+
 formElement.addEventListener("submit", async (e) => {
+  inputElement.disabled = true;
+  buttonElement.disabled = true;
+
   e.preventDefault();
   const original_url = e.target.elements.original_url.value;
   await onShortendUrl(original_url);
+
+  inputElement.disabled = false;
+  buttonElement.disabled = false;
 });
 
 const onShortendUrl = async (original_url) => {
+  shortendElement.style.opacity = 100;
   original_url = original_url.trim();
 
   if (original_url.length <= 0) {
@@ -17,7 +31,27 @@ const onShortendUrl = async (original_url) => {
     body: JSON.stringify({ Url: original_url }),
   });
 
-  console.log(res);
+  if (res.status != 201) {
+    console.error(res);
+    return;
+  }
+
   const jsonData = await res.json();
-  console.log(jsonData);
+  shortendTextElement.innerText = jsonData.ShortendUrl;
 };
+
+const copyUrl = (e) => {
+  if (!navigator.clipboard) {
+    alert("Your browser doesn't support copying to clipboard.");
+    return;
+  }
+
+  navigator.clipboard.writeText(e.target.innerText).then(
+    function () {},
+    function (err) {
+      console.error("failed copying.", err);
+    },
+  );
+};
+
+shortendElement.addEventListener("click", copyUrl);
