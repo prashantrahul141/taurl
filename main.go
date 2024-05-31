ec301bc00d3b2455fef0c091ae69fa61fcc3fca0
Prashant <prashantrahul141@protonmail.com>
2024-05-31 12:13:35 +0530

    make config in main

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import "taurl/app"
 
+var GlobalConfig app.Configuration = app.InitConfig()
+
 func main() {
-	app := app.Default()
-	app.Run()
+	app := app.Default(GlobalConfig)
+	app.Run(GlobalConfig.Port)
 }
